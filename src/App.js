32a449e94cca82749380af36f9b496195a02ea88
Aaron Melocik <aaron@worldsingles.com>
2017-09-02 23:18:55 -0700

    Add material ui theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import CardExample from './CardExample';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+          <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+
+        <RaisedButton label="Default">RaisedButton Content</RaisedButton>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <CardExample />
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Using themes: http://www.material-ui.com/#/customization/themes
+const ThemedApp = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<ThemedApp />, document.getElementById('root'));
 registerServiceWorker();
