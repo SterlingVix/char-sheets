e9444a428f4d43c6178cd2c1d337e2152ba51ccc
Aaron Melocik <aaron@worldsingles.com>
2017-10-07 21:44:32 -0700

    Adding firebase setup.

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// <script src="https://www.gstatic.com/firebasejs/4.5.0/firebase.js"></script>
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyC6UoE5TzbHzs-CV867tOCXOiF1d6eg58E",
+  authDomain: "sterling-vix-char-sheets.firebaseapp.com",
+  databaseURL: "https://sterling-vix-char-sheets.firebaseio.com",
+  projectId: "sterling-vix-char-sheets",
+  storageBucket: "",
+  messagingSenderId: "556363376751"
+};
+firebase.initializeApp(config);
